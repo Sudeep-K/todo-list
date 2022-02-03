8f5c632a7b4bd26d53b49c069ab9cda7c1390fe2
Sudeep-K <pong32btl@gmail.com>
2022-02-03 19:20:49 +0545

    final commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# TODO-LIST
+
+todo-list is a simple todo app created using js and webpack
+
+### 👋 Why this project ?
+
+- simple implementation of todo app using webpack and npm usage
+- implementation of bundle management of js files using webpack
+- part of assignment from [theodinproject.com]
+
+### 📚 What did I learn ?
+
+- modules and libraries in javascript
+- implementation of important steps of problem solving
+- dynamic content generation in JS
+- using event handlers and query selector in JS
+
+### 🌎 Future updates
+
+> ability to add new projects coming soon
+
+> 🌐 check out the project now @[https://sudeep-k.github.io/todo-list/]
+
---
+++ src/nextweektodo.js
@@ -0,0 +1,34 @@
+import differenceInCalendarDays from "date-fns/differenceInCalendarDays";
+import { displaytodo } from "./todotoday";
+
+function viewtodonextweek() {
+    /* query selector */
+    const greetings_span = document.querySelector("#greetings span");
+    let todo_list = document.querySelector("#todo-list");
+
+    /* clear the todo-list div for appending new elements */
+    todo_list.textContent = "";
+
+    /* greetings to be today */
+    greetings_span.textContent = "Next 7 Days";
+
+    const today = new Date();
+
+    let todo_objs;
+
+    if (localStorage.getItem("todo_objs") === null) {
+        todo_objs = [];
+    } else {
+        todo_objs = JSON.parse(localStorage.getItem("todo_objs"));
+    }
+
+    todo_objs.forEach(element => {
+        let element_date = new Date(element.date);
+        let difference_days = differenceInCalendarDays(today, element_date);
+        if (difference_days > 0 && difference_days < 8) {
+            displaytodo(element);
+        }
+    });
+}
+
+export {viewtodonextweek};
